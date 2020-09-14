29b50ef419f2e79a7a10cd756821c593e6ef6bca
Laura.Foulquier <laura.foulquier@LauraFouiersMBP.home>
2020-09-14 09:36:29 +0100

    final submission

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=laurafoulquiershell/lfmicroservices:1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+# kubectl run predictions --image=$dockerpath --port 80
+kubectl run --generator=run-pod/v1 --image=$dockerpath predictions
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predictions-695dfdb6fc-mh5tw 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=laurafoulquiershell/lfmicroservices:1.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/docker_password.txt | docker login --username laurafoulquiershell --password-stdin 
 
 # Step 3:
 # Push image to a docker repository
+docker tag 39348b15d23c $dockerpath
+docker push $dockerpath
